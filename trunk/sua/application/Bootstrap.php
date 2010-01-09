a722e5c41065920a52656a5b69b8a37a13730a48
peter@smitmail.eu <peter@smitmail.eu@6cee73fc-d593-11dd-8ca8-e3ff79f713b6>
2010-01-09 09:50:11 +0000

    SUA: made ready for demo

--- trunk/sua/application/Bootstrap.php
+++ trunk/sua/application/Bootstrap.php
@@ -11,6 +11,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         return $autoloader;
     }
     
+    
     protected function _initProfiler()
     {
         $profiler = new Zend_Db_Profiler_Firebug('All DB Queries');
@@ -26,6 +27,30 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     protected function _initTimeZone()
     {
         date_default_timezone_set('UTC');
+    }
+    
+    public function _initDoctrine()
+    {
+    	require_once('Doctrine/Core.php');
+    	$this->getApplication()->getAutoloader()
+             ->pushAutoloader(array('Doctrine_Core', 'autoload'));
+        spl_autoload_register(array('Doctrine_Core', 'modelsAutoload'));
+        
+        $manager = Doctrine_Manager::getInstance();
+        $manager->setAttribute(Doctrine_Core::ATTR_AUTO_ACCESSOR_OVERRIDE, true);
+        $manager->setAttribute(
+          Doctrine_Core::ATTR_MODEL_LOADING,
+          Doctrine_Core::MODEL_LOADING_CONSERVATIVE
+        );
+        $manager->setAttribute(Doctrine_Core::ATTR_AUTOLOAD_TABLE_CLASSES, true);
+
+        $doctrineConfig = $this->getOption('doctrine');
+
+        Doctrine_Core::loadModels($doctrineConfig['models_path']);
+       
+        $conn = Doctrine_Manager::connection($doctrineConfig['dsn'],'doctrine');
+        $conn->setAttribute(Doctrine_Core::ATTR_USE_NATIVE_ENUM, true);
+     return $conn;
     }
     
 
