d66849fdcca65b5c64ce06013223938d5a6ce0d8
peter@smitmail.eu <peter@smitmail.eu@6cee73fc-d593-11dd-8ca8-e3ff79f713b6>
2010-01-18 14:06:57 +0000

    SUA bpms: updated to new requirements

--- sua_bpms/public/sampleForm.php
+++ sua_bpms/public/sampleForm.php
@@ -7,36 +7,52 @@
 <script type="text/javascript" src="js/jquery.ba-postmessage.min.js"></script>
 
 <script type="text/javascript">
-var sua_url = "";
+var sua_url = "http://mini-challenge";
 function clearFileUpload(divid) {
-	$('#'+divid+'-filename').value = '';
-    $('#'+divid+'-fileid').value = '';
+	$('#'+divid+'-filename').val('');
+    	$('#'+divid+'-fileid').val('');
 	$('#'+divid+'-thumbnail').html("");
-	$('#'+divid+'-choosefile').disabled = false;
+	$('#'+divid+'-title').removeAttr("disabled"); 
+	$('#'+divid+'-description').removeAttr("disabled"); 
+	$('#'+divid+'-choosefile').removeAttr("disabled"); 
 }
 $(document).ready(function(){
 	$.receiveMessage(
         function(e){
             var parts = e.data.split('|', 3);
+            var divid = parts[0];
+			$.modal.close();
             if(parts[1] == "") {
 				clearFileUpload(parts[0]);
-			}
-            if(parts[1] != "") {
-				$('#'+divid+'-filename').value = parts[2];
-    			$('#'+divid+'-fileid').value = parts[1];
+			} else {
+				$('#'+divid+'-filename').val(parts[2]);
+    			$('#'+divid+'-fileid').val(parts[1]);
 				$('#'+divid+'-thumbnail').html("<img src='"+sua_url+"/thumbnails/"+parts[2]+"' alt=''>");
-				$('#'+divid+'-choosefile').disabled = true;
+				$('#'+divid+'-title').attr("disabled", true);
+				$('#'+divid+'-description').attr("disabled", true);
+				$('#'+divid+'-choosefile').attr("disabled", true);
 	            //$("#"+parts[0]).html("<p>There is returned: "+parts[1]+"</p><input type='hidden' id='input-"+parts[0]+"' name='input-"+parts[0]+"' value='"+parts[1]+"' ");
             }
-            $.modal.close()
+            
 	    },
 	    sua_url
 	);
 
     $(".file-choose-button").click(
         function(event){
+			event.preventDefault();
+            var divid = event.target.parentNode.id;
+
+            var title = $('#' + divid + '-title').val();
+            var description = $('#'+ divid + '-description').val();
+
+			if(title == '' || description == '') {
+				alert("Please give a title and description before uploading the file");
+				return;
+			}
+
             var iframe_url = $.ajax({
-                url: "makeFileId.php",
+                url: "makeFileId.php?form_pending_id="+encodeURIComponent($('#form_pending_id').val())+'&title=' + encodeURIComponent(title) + '&description=' + encodeURIComponent(description),
                 async: false
             }).responseText;
             
@@ -44,7 +60,7 @@ $(document).ready(function(){
             $.modal('<iframe src="' + iframe_url + '&amp;return_element=' + event.target.parentNode.id +'&amp;filetype=image&amp;return_url=' + encodeURI(window.location)+ '" height="450" width="450" style="border:0">', {
 	            closeHTML:"",
 	            containerCss:{
-		            backgroundColor:"#123564",
+		            backgroundColor:"#ddd",
 		            borderColor:"#0063dc",
 		            height:450,
 		            padding:0,
@@ -52,7 +68,7 @@ $(document).ready(function(){
 	            },
 	            overlayClose:true
             });
-            event.preventDefault();
+            
         }
 	);
 
@@ -77,10 +93,13 @@ $(document).ready(function(){
 
 <h2>A form</h2>
 <form method='post' action='showsubmit.php'>
+<input type='hidden' id='form_pending_id' name='form_pending_id' value='<?php echo rand(100000, 999999); ?>'>
 <div id="headimage">
   <h2>Head Image</h2>
-  <input type='text' id='headimage-filename' value='' DISABLED>
-  <input type='hidden' id='headimage-fileid' value=''>
+  <input type='text' id='headimage-filename' name='headimage-filename' value='' DISABLED>
+  <label for="headimage-title">Title</label><input type='text' id='headimage-title' name='headimage-title' value=''>
+  <label for="headimage-description">Description</label><input type='text' id='headimage-description' name='headimage-description' value=''>
+  <input type='hidden' id='headimage-fileid' name='headimage-fileid' value=''>
   <input type='submit' id='headimage-choosefile' value='Choose' class='file-choose-button'>
   <input type='reset' id='headimage-reset' value='Clear' class='file-reset-button'>
   <div id='headimage-thumbnail'></div>
@@ -88,8 +107,10 @@ $(document).ready(function(){
 
 <div id="normalimage">
   <h2>Normal Image</h2>
-  <input type='text' id='normalimage-filename' value='' DISABLED>
-  <input type='hidden' id='normalimage-fileid' value=''>
+  <input type='text' id='normalimage-filename' name='normalimage-filename' value='' DISABLED>
+  <label for="normalimage-title">Title</label><input type='text' id='normalimage-title' name='normalimage-title' value=''>
+  <label for="normalimage-description">Description</label><input type='text' id='normalimage-description' name='normalimage-description' value=''>
+  <input type='hidden' id='normalimage-fileid' name='normalimage-fileid' value=''>
   <input type='submit' id='normalimage-choosefile' value='Choose' class='file-choose-button'>
   <input type='reset' id='normalimage-reset' value='Clear' class='file-reset-button'>
   <div id='normalimage-thumbnail'></div>
