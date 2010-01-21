04a48b5b6f395e2ba8930892ae4730337bba0343
peter@smitmail.eu <peter@smitmail.eu@6cee73fc-d593-11dd-8ca8-e3ff79f713b6>
2010-01-21 07:52:57 +0000

    SUA: Added video support

--- sua/library/So/Fileprocessor/Action/Thumbnail.php
+++ sua/library/So/Fileprocessor/Action/Thumbnail.php
@@ -12,12 +12,28 @@ class So_Fileprocessor_Action_Thumbnail extends So_Fileprocessor_Action {
 					$this->_options['target_path'].'/'.$work->file_name, 
 					$work->file_mimetype
 				);
+				
+				$work->thumbnail_file_name = $work->file_name;
+				$work->save();
 				break;
 			case "video":
-				throw new Exception("Video Thumbnailing not Implemented yet");
-//				$mediaConverter->makeVideoThumbnail();
+				$thumnailFileName = $this->_getNewFilename($work->file_name, 'jpg');
+				
+				$mediaConverter->makeVideoThumbnail(
+					$this->_options['source_path'].'/'.$work->file_name, 
+					$this->_options['target_path'].'/'.$thumnailFileName
+				);
+				
+				$work->thumbnail_file_name = $thumnailFileName;
+				$work->save();
 				break;
 		}
 		$this->_success = true;
+	}
+	
+	private function _getNewFilename($oldFilename, $newExtension) {
+		$dotLocation = strripos($oldFilename, '.');
+		if(!$dotLocation) return $oldFilename. '.' . $newExtension;
+		else return substr($oldFilename, 0, $dotLocation). '.' . $newExtension;
 	}
 }
